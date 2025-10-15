3e0b33518e139c5210fae23ea56947b670129efa
Eshaan Negi <11eshaansinghnegi@gmail.com>
2025-10-15 19:33:52 +0530

    basic stuff

---
+++ Logical Operators/script.js
@@ -0,0 +1,18 @@
+const age = 20
+const gender = 'male';
+
+
+//AND 
+//all the conditions must be true
+if(age>=18 && gender=='male'){
+    console.log('Your are adult male')
+}
+
+
+
+//OR
+//
+
+if(age>=18 || gender=='female'){
+    console.log('Your ')
+}
