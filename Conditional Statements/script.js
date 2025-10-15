f67343d876f405d37232c7fe5e29d967b18f1f46
Eshaan Negi <11eshaansinghnegi@gmail.com>
2025-10-15 19:04:03 +0530

    if-else and switch case

---
+++ Conditional Statements/script.js
@@ -0,0 +1,14 @@
+const age=98
+
+if(age>=18){
+    console.log("Yes,you can vote")
+}else{
+    console.log("No,u cant vote")
+}
+
+
+//ternary operator
+//only used if i have 2 conditons 
+
+const legal=12
+legal>=21 ? console.log('Yes') : console.log("no")
