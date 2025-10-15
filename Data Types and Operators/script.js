d99a12e19436599b35b0714a2136f70b430bed2b
Eshaan Negi <11eshaansinghnegi@gmail.com>
2025-10-15 18:33:25 +0530

    learned the topic

---
+++ Data Types and Operators/script.js
@@ -0,0 +1,53 @@
+//data types
+
+//numbers
+//implicit typecasting
+
+let num=4.1;
+
+console.log(num+1)
+
+
+//strings
+const snumber="123"       //even numbers can be stored
+const firstName='eshaan'
+console.log(snumber)
+console.log(firstName)
+
+//booleans
+let isLoggesdIn = true
+console.log(isLoggesdIn)
+
+//Null
+let lastLoginDate= null
+console.log(lastLoginDate)
+
+
+//objects
+const person = {
+    firstName: 'Eshaan',
+    lastName: 'Negi',
+    age:22,
+    isLoggesdIn: false
+}
+
+console.log(person)
+console.log(person.age)     //if i want to see a specific thing
+
+
+//bonus for booleans
+//js consider 
+//true ->1
+//false ->0
+let question=true;
+console.log(question+1)
+
+//if we multiply a string and a number, we get number
+//string + string = string
+//string + number = string
+//string * number = number
+console.log('123'*1)
+
+//typeof operator
+
+console.log(typeof '123')
