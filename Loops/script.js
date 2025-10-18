9126ea5f7b70aca5e6f99a70afa738b3050c2228
Eshaan Negi <11eshaansinghnegi@gmail.com>
2025-10-18 12:27:37 +0530

    basic loops

---
+++ Loops/script.js
@@ -0,0 +1,23 @@
+//for loop->> if you know how many times to loop
+
+for(let i =1;i<=10;i++){
+    console.log('eshaan'+i)
+}
+
+//while loop->we know the condition, but pata nahi kitne mei hoga
+
+
+let ip=0;
+let house=100;
+
+// while(ip!=house){
+//     ip++
+//     console.log('Step taken '+ip)
+// }
+
+//do while->phele run karega then will check the condition
+
+do{ 
+    ip++;
+    console.log('step taken'+ip)
+}while(ip<house)
