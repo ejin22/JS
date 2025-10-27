e633b11608fdef2622c6cf547a9da22ed3bd6f87
Eshaan Negi <11eshaansinghnegi@gmail.com>
2025-10-27 17:27:30 +0530

    just checking

--- Arrow Functions/script.js
+++ Arrow Functions/script.js
@@ -1,10 +1,36 @@
-//syntax
 
-const sayHello= () => {
-    console.log('hey')
+
+const sayHello = () => {
+    console.log('hello')
 }
-dsfad
+
+sayHello();
+
+const add = (a, b) => {
+    return a+b
+}
+
+console.log(add(2,3))
+
+//we could also write the above statement as 
+
+const Add=(a,b) => a+b
+console.log(add(1,2))       //CALLED ONE LINER APPROACH
+//used when only 1 line is used
 
 
-sayHello()
+
+//HOISTING 
+
+mhmm();
+
+function mhmm(){
+    console.log('heell')
+}
+
+//here i called the function b4 it was delcared 
+
+
+
+//this keywoord
 
